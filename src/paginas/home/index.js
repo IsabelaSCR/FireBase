4880dabda81692c28ef26e5ae7580de9c2731c34
EnzRB <enzzomoletomroxo@gmail.com>
2022-09-20 10:53:48 -0300

    firebase

---
+++ src/paginas/home/index.js
@@ -0,0 +1,54 @@
+import { useNavigation } from '@react-navigation/native';
+import firebase from '../../firebaseConnection'
+import React, {useState,useEffect} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity,Button} from 'react-native';
+
+
+
+
+
+export default function Home(){
+const [nome, setNome] = useState('');
+
+useEffect(()=>{
+
+
+    async function buscarNome(){
+
+
+        await firebase.database().ref('Alunos/1/Nome:').on('value',(snapshot) =>{
+                
+            setNome(snapshot.val())
+    });
+}
+        buscarNome();
+
+},[])
+
+
+
+return(
+
+    <View style = {{alignItems:'center'}}>
+
+      <Text>{nome} </Text>
+            
+    </View>
+
+
+);
+
+
+}
+
+const styles = StyleSheet.create({
+
+    botao:{
+        backgroundColor:'green',
+        width:'80%',
+        alignItems:"center",
+        marginTop:10
+    }
+
+}
+)
